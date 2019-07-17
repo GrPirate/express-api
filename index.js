24645610f17c3411e99b2dc43ce68d5b8fe204ff
ruilip <735061202@qq.com>
2019-07-17 18:17:57 +0800

    设置跨域

--- index.js
+++ index.js
@@ -78,6 +78,15 @@ app.use(function (err, req, res, next) {
   req.flash('error', err.message)
   // res.redirect('/posts')
 })
+// 跨域访问
+app.all('*', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With')
+  res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS')
+  res.header('X-Powered-By', ' 3.2.1')
+  res.header('Content-Type', 'application/json;charset=utf-8')
+  next()
+})
 
 // 监听端口，启动程序
 app.listen(config.port, config.hostname, function () {
