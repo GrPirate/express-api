f595f14bc0f60c9b8d71e0b85da920d8195232a9
ruilip <735061202@qq.com>
2019-07-18 10:01:13 +0800

    80

--- config/production.js
+++ config/production.js
@@ -1,5 +1,5 @@
 module.exports = {
-  port: 3001,
-  origin: ['http://47.105.189.200:3001'],
+  port: 80,
+  origin: ['http://47.105.189.200:3000'],
   mongodb: 'mongodb://47.105.189.200:27017/myapi'
 }
